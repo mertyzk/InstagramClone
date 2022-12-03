f674748ea75d66c5de866be5b40743c32225027e
RinKusChi <meyuzak@gmail.com>
2022-12-03 15:19:00 +0300

    Upload Post UI Completed

--- InstagramFirebaseWork/InstagramFirebaseWork/Controller/PostUpload/UploadPostVC.swift
+++ InstagramFirebaseWork/InstagramFirebaseWork/Controller/PostUpload/UploadPostVC.swift
@@ -10,22 +10,56 @@ import UIKit
 final class UploadPostVC: UIViewController {
     
     
-    //MARK: - Properties
+    //MARK: - UI Elements
+    private lazy var photoImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = 10
+        imageView.image = #imageLiteral(resourceName: "venom-7")
+        return imageView
+    }()
     
+    private lazy var captionTextView: UITextView = {
+        let textView = UITextView()
+        textView.layer.cornerRadius = 10
+        textView.layer.borderWidth = 0.3
+        textView.layer.borderColor = UIColor.lightGray.cgColor
+        return textView
+    }()
+    
+    private lazy var characterCountLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .lightGray
+        label.font      = UIFont.systemFont(ofSize: 14)
+        label.text      = "0/100"
+        return label
+    }()
     
     //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureUI()
+        configureUploadPostVC()
+        configureUIElements()
     }
     
     
     //MARK: - Helpers
-    private func configureUI() {
+    private func configureUploadPostVC() {
         view.backgroundColor              = .white
         navigationItem.title              = "Upload Post"
         navigationItem.leftBarButtonItem  = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(cancelButtonPressed))
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Share", style: .done, target: self, action: #selector(shareButtonPressed))
+    }
+    
+    
+    private func configureUIElements() {
+        view.addSubviewsExt(photoImageView, captionTextView, characterCountLabel)
+        photoImageView.setDimensions(height: 240, width: 240)
+        photoImageView.anchor(top: view.safeAreaLayoutGuide.topAnchor, paddingTop: 5)
+        photoImageView.centerX(inView: view)
+        captionTextView.anchor(top: photoImageView.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 15, paddingLeft: 12, paddingRight: 12, height: 64)
+        characterCountLabel.anchor(top: captionTextView.bottomAnchor, right: captionTextView.rightAnchor, paddingTop: 5)
     }
     
     
